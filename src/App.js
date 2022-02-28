c3dbb9fc004da673da17da7937070e6d3971f0d8
camilogutierrezruiz <camilogutierrez593@hotmail.com>
2022-02-27 19:45:17 -0500

    Page logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import React from 'react';
 import './App.css';
 import QuoteCard from './components/QuoteCard.jsx';
+import Logo from './assets/Logo.svg'
 
 function App() {
 
   return (
     <div className="App">
+      <div className='logo'>
+        <div className='logo__wrapper'>
+          <img src={Logo} alt="" />
+        </div>
+        <h1>Quotes Generator</h1>
+      </div>
       <QuoteCard />
     </div>
   );
